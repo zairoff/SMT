ce83050fd7525810137868d232d39675a3668a7a
marufjon_zoirov <marufjon_zoirov@epam.com>
2021-10-16 10:53:51 +0500

    init

---
+++ SMT.Api/Extensions/ServiceExtension.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SMT.Access;
+using SMT.Services;
+using SMT.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SMT.Api.Extensions
+{
+    public static class ServiceExtension
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            services.AddScoped<IPcbReportService, PcbReportService>();
+            return services;
+        }
+    }
+}
