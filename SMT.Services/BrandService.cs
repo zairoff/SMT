faa4fd4922e8b110ea34c730aa00ff6ad40cf913
marufjon_zoirov <marufjon_zoirov@epam.com>
2021-10-21 12:10:43 +0500

    implemented BrandService

---
+++ SMT.Services/BrandService.cs
@@ -0,0 +1,88 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using SMT.Access;
+using SMT.Common.Dto.BrandDto;
+using SMT.Common.Exceptions;
+using SMT.Domain;
+using SMT.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.Services
+{
+    public class BrandService : IBrandService
+    {
+        private readonly IRepository<Brand> _repository;
+        private readonly IMapper _mapper;
+
+        public BrandService(IRepository<Brand> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<BrandResponse> AddAsync(BrandCreate brandCreate)
+        {
+            var brand = await _repository.Get().Where(p => p.Name == brandCreate.Name).FirstOrDefaultAsync();
+
+            if (brand != null)
+                throw new ConflictException();
+
+            brand = _mapper.Map<BrandCreate, Brand>(brandCreate);
+
+            await _repository.AddAsync(brand);
+
+            return _mapper.Map<Brand, BrandResponse>(brand);
+        }
+
+        public async Task<BrandResponse> DeleteAsync(int id)
+        {
+            var brand = await _repository.Get().Where(p => p.Id == id).FirstOrDefaultAsync();
+
+            if (brand == null)
+                throw new NotFoundException();
+
+            await _repository.DeleteAsync(brand);
+
+            return _mapper.Map<Brand, BrandResponse>(brand);
+        }
+
+        public async Task<IEnumerable<BrandResponse>> GetAllAsync()
+        {
+            var brands = await _repository.GetAll().ToListAsync();
+
+            return _mapper.Map<IEnumerable<Brand>, IEnumerable<BrandResponse>>(brands);
+        }
+
+        public async Task<BrandResponse> GetAsync(int id)
+        {
+            var brand = await _repository.Get().Where(p => p.Id == id).FirstOrDefaultAsync();
+
+            return _mapper.Map<Brand, BrandResponse>(brand);
+        }
+
+        public async Task<BrandResponse> GetByNameAsync(string name)
+        {
+            var brand = await _repository.Get().Where(p => p.Name == name).FirstOrDefaultAsync();
+
+            return _mapper.Map<Brand, BrandResponse>(brand);
+        }
+
+        public async Task<BrandResponse> UpdateAsync(int id, BrandUpdate brandUpdate)
+        {
+            var brand = await _repository.Get().Where(p => p.Id == id).FirstOrDefaultAsync();
+
+            if (brand == null)
+                throw new NotFoundException();
+
+            brand.Name = brandUpdate.Name;
+
+            await _repository.UpdateAsync(brand);
+
+            return _mapper.Map<Brand, BrandResponse>(brand);
+        }
+    }
+}
--- SMT.Services/Interfaces/IBrandService.cs
+++ SMT.Services/Interfaces/IBrandService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SMT.Common.Dto.BrandDto;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,16 @@ namespace SMT.Services.Interfaces
 {
     public interface IBrandService
     {
+        Task<IEnumerable<BrandResponse>> GetAllAsync();
 
+        Task<BrandResponse> GetAsync(int id);
+
+        Task<BrandResponse> GetByNameAsync(string name);
+
+        Task<BrandResponse> AddAsync(BrandCreate brandCreate);
+
+        Task<BrandResponse> UpdateAsync(int id, BrandUpdate brandUpdate);
+
+        Task<BrandResponse> DeleteAsync(int id);
     }
 }
