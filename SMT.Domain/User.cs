ef48ec1173bc9ef739275e45c59ea857e250a014
zairoff <marufzoiroff@gmail.com>
2021-10-29 09:25:18 +0500

    added Security project

---
+++ SMT.Domain/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.Domain
+{
+    public enum Role
+    {
+        User,
+        Brigadir,
+        Manager,
+        Admin
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public Role Role { get; set; }
+
+        public string Telegram { get; set; }
+    }
+}
