d10cbe827d86221b0271d5d216d8d04a795d00d4
zairoff <marufzoiroff@gmail.com>
2021-10-29 09:33:52 +0500

    added UserDto

---
+++ SMT.Common/Dto/UserDto/UserCreate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.Common.Dto.UserDto
+{
+    public class UserCreate
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Role { get; set; }
+
+        public string Telegram { get; set; }
+    }
+}
---
+++ SMT.Common/Dto/UserDto/UserResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.Common.Dto.UserDto
+{
+    public class UserResponse
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+    }
+}
