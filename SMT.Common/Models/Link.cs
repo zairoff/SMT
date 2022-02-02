c8ed0e65fd32dd00acbef3e134b77fa38f01dc79
Maruf <marufzoiroff@gmail.com>
2022-02-02 10:16:18 +0500

    project name changed smt.common to smt.viewmodel

---
+++ SMT.Common/Models/Link.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMT.Common.Models
+{
+    public class Link
+    {
+        public const string GetMethod = "Get";
+
+        public static Link To(string routeName, object routeValues = null) => new()
+        {
+            RouteName = routeName,
+            RouteValues = routeValues,
+            Method = GetMethod
+        };
+
+        [JsonProperty(Order = -4)]
+        public string Href { get; set; }
+
+        [JsonProperty(Order = -3, PropertyName = "rel", NullValueHandling = NullValueHandling.Ignore)]
+        public string[] Relations { get; set; }
+
+        [JsonProperty(Order = -2, DefaultValueHandling = DefaultValueHandling.Ignore, NullValueHandling = NullValueHandling.Ignore)]
+        [DefaultValue(GetMethod)]
+        public string Method { get; set; }
+
+        [JsonIgnore]
+        public string RouteName { get; set; }
+
+        [JsonIgnore]
+        public object RouteValues { get; set; }
+    }
+}
