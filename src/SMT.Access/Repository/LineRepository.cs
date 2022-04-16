7547ab4945f0e1ceb74aa8d8affcdb00074538e8
Maruf <marufzoiroff@gmail.com>
2022-04-16 14:30:42 +0500

    created LineRepository

---
+++ src/SMT.Access/Repository/LineRepository.cs
@@ -0,0 +1,14 @@
+﻿using SMT.Access.Data;
+using SMT.Access.Repository.Base;
+using SMT.Access.Repository.Interfaces;
+using SMT.Domain;
+
+namespace SMT.Access.Repository
+{
+    public class LineRepository : BaseRepository<Line>, ILineRepository
+    {
+        public LineRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
