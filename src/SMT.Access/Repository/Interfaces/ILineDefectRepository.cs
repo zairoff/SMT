f203e4696d22c5883857ef0e0b821cff7728da80
Maruf <marufzoiroff@gmail.com>
2022-04-16 15:04:04 +0500

    Created & implemented LineDefectRepository

---
+++ src/SMT.Access/Repository/Interfaces/ILineDefectRepository.cs
@@ -0,0 +1,14 @@
+﻿using SMT.Access.Repository.Base;
+using SMT.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SMT.Access.Repository.Interfaces
+{
+    public interface ILineDefectRepository : IBaseRepository<LineDefect>
+    {
+        Task<IEnumerable<LineDefect>> GetByAsync(Expression<Func<LineDefect, bool>> expression);
+    }
+}
---
+++ src/SMT.Access/Repository/LineDefectRepository.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SMT.Access.Data;
+using SMT.Access.Repository.Base;
+using SMT.Access.Repository.Interfaces;
+using SMT.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace SMT.Access.Repository
+{
+    public class LineDefectRepository : BaseRepository<LineDefect>, ILineDefectRepository
+    {
+        public LineDefectRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async override Task<LineDefect> FindAsync(Expression<Func<LineDefect, bool>> expression)
+        {
+            return await DbSet.Include(p => p.Line).Include(p => p.Defect).Where(expression).FirstOrDefaultAsync();
+        }
+
+        public async override Task<IEnumerable<LineDefect>> GetAllAsync()
+        {
+            return await DbSet.Include(p => p.Line).Include(p => p.Defect).ToListAsync();
+        }
+
+        public async Task<IEnumerable<LineDefect>> GetByAsync(Expression<Func<LineDefect, bool>> expression)
+        {
+            return await DbSet.Where(expression).Include(p => p.Line).Include(p => p.Defect).ToListAsync();
+        }
+    }
+}
