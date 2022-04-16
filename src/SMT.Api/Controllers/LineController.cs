bf534af0ebe6b48e45f8e13b29fdae6f0f465e21
Maruf <marufzoiroff@gmail.com>
2022-04-16 14:35:02 +0500

    implemented LineController

---
+++ src/SMT.Api/Controllers/LineController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SMT.Services.Interfaces;
+using SMT.ViewModel.Dto.LineDto;
+using System.Threading.Tasks;
+
+namespace SMT.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LineController : BaseController
+    {
+        private readonly ILineService _service;
+
+        public LineController(ILineService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _service.GetAllAsync();
+
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var result = await _service.GetAsync(id);
+
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("GetByName")]
+        public async Task<IActionResult> GetByName(string name)
+        {
+            var result = await _service.GetByNameAsync(name);
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateReport([FromBody] LineCreate lineCreate)
+        {
+            var result = await _service.AddAsync(lineCreate);
+
+            return CreatedAtAction(nameof(Get), new { id = result.Id }, result);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> UpdateReport(int id, [FromBody] LineUpdate lineUpdate)
+        {
+            var result = await _service.UpdateAsync(id, lineUpdate);
+
+            return Ok(result);
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> DeleteReport(int id)
+        {
+            var result = await _service.DeleteAsync(id);
+
+            return Ok(result);
+        }
+    }
+}
