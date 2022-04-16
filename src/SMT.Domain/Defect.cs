e24b207a6fb2bbaed8cfe51d9a24a3c8efa1e0a6
Maruf <marufzoiroff@gmail.com>
2022-04-16 10:28:30 +0500

    removed Line propery from Defects model

--- src/SMT.Domain/Defect.cs
+++ src/SMT.Domain/Defect.cs
@@ -4,10 +4,6 @@
     {
         public int Id { get; set; }
 
-        public int LineId { get; set; }
-
-        public virtual Line Line { get; set; }
-
         public string Name { get; set; }
     }
 }
