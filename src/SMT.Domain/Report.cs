4f473a9ffc9460ed2551453369dee098eeaca59f
Maruf <marufzoiroff@gmail.com>
2022-04-28 14:34:28 +0500

    added Reports entity

---
+++ src/SMT.Domain/Report.cs
@@ -0,0 +1,19 @@
+﻿namespace SMT.Domain
+{
+    public class Report
+    {
+        public int Id { get; set; }
+
+        public int LineId { get; set; }
+
+        public Line Line { get; set; }
+
+        public int DefectId { get; set; }
+
+        public Defect Defect { get; set; }
+
+        public int ModelId { get; set; }
+
+        public Model Model { get; set; }
+    }
+}
