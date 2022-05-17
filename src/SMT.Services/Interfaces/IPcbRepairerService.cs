837ae0c6d5b312fc0670dbace35c6cac203ddd2c
Maruf <marufzoiroff@gmail.com>
2022-05-17 11:33:13 +0500

    completed MachineRepairer feature

---
+++ src/SMT.Services/Interfaces/IPcbRepairerService.cs
@@ -0,0 +1,17 @@
+﻿using SMT.ViewModel.Dto.RepairerDto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SMT.Services.Interfaces
+{
+    public interface IPcbRepairerService
+    {
+        Task<IEnumerable<RepairerResponse>> GetAllAsync();
+
+        Task<RepairerResponse> GetAsync(int id);
+
+        Task<RepairerResponse> AddAsync(RepairerCreate repairerCreate);
+
+        Task<RepairerResponse> DeleteAsync(int id);
+    }
+}
---
+++ src/SMT.ViewModel/Dto/MachineRepairDto/MachineRepairResponse.cs
@@ -0,0 +1,22 @@
+﻿using SMT.ViewModel.Dto.EmployeeDto;
+using SMT.ViewModel.Dto.MachineDto;
+
+namespace SMT.ViewModel.Dto.MachineRepairDto
+{
+    public class MachineRepairResponse
+    {
+        public int Id { get; set; }
+
+        public MachineResponse Machine { get; set; }
+
+        public EmployeeResponse Employee { get; set; }
+
+        public string Issue { get; set; }
+
+        public string Action { get; set; }
+
+        public string NotificationDate { get; set; }
+
+        public string CreatedDate { get; set; }
+    }
+}
