cbdef15b32019b7bfd2b46f8f408cd1f132c4a5e
zairoff <marufzoiroff@gmail.com>
2023-11-06 15:11:49 +0500

    repository

---
+++ src/SMT.Access/Repository/Interfaces/IReadyProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using SMT.Access.Repository.Base;
+using SMT.Domain;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using System;
+
+namespace SMT.Access.Repository.Interfaces
+{
+    public interface IReadyProductRepository : IBaseRepository<ReadyProduct>
+    {
+        Task<IEnumerable<ReadyProduct>> GetByAsync(Expression<Func<ReadyProduct, bool>> expression);
+    }
+}
---
+++ src/SMT.Access/Repository/ReadyProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using SMT.Access.Data;
+using SMT.Access.Repository.Base;
+using SMT.Access.Repository.Interfaces;
+using SMT.Domain;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Linq;
+using System.Threading.Tasks;
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SMT.Access.Repository
+{
+    public class ReadyProductRepository : BaseRepository<ReadyProduct>, IReadyProductRepository
+    {
+        public ReadyProductRepository(AppDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<ReadyProduct>> GetByAsync(Expression<Func<ReadyProduct, bool>> expression)
+        {
+            return await DbSet.Where(expression).OrderBy(x => x.Enter).ToListAsync();
+        }
+    }
+}
