5a7331828cf4c479bfbd94a5a5e8e3f9f25fe32e
zairoff <marufzoiroff@gmail.com>
2023-11-06 15:11:05 +0500

    create domain

---
+++ src/SMT.Domain/ReadyProduct.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SMT.Domain
+{
+    public class ReadyProduct
+    {
+        public int Id { get; set; }
+
+        public int ModelId { get; set; }
+
+        public Model Model { get; set; }
+
+        public int Count { get; set; }
+
+        public bool Inside { get; set; }
+
+        public DateTime Enter { get; set; }
+
+        public DateTime Exit { get; set; }
+    }
+}
