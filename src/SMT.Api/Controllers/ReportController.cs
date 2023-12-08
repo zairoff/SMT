d662e837b68cc297cae19c8decbc6bc8630fed87
zairoff <marufzoiroff@gmail.com>
2023-12-08 14:11:27 +0500

    fix the issue

--- src/SMT.Api/Controllers/ReportController.cs
+++ src/SMT.Api/Controllers/ReportController.cs
@@ -125,14 +125,14 @@ namespace SMT.Api.Controllers
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> UpdateReport(int id, [FromBody] ReportUpdate reportUpdate)
         {
-            if (string.IsNullOrEmpty(reportUpdate.Action) || string.IsNullOrEmpty(reportUpdate.Condition))
+            if (reportUpdate.Status == true && string.IsNullOrEmpty(reportUpdate.Action) || string.IsNullOrEmpty(reportUpdate.Condition))
             {
-                return new BadRequestResult();
+                return new BadRequestObjectResult("xolatini yozing");
             }
 
-            if (reportUpdate.Action.Length  < 2 || reportUpdate.Condition.Length < 2)
+            if (reportUpdate.Status == true &&  reportUpdate.Action.Length  < 2 || reportUpdate.Condition.Length < 2)
             {
-                return new BadRequestResult();
+                return new BadRequestObjectResult("xolatini yozing");
             }
 
             var result = await _service.UpdateAsync(id, reportUpdate);
